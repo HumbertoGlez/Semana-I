d92bdf261aa6e9736057942fd7826b0358f72449
Humberto Gonzalez <hglez.98s@gmail.com>
2019-10-30 10:31:38 -0600

    Cleaned up review intent code

--- index.js
+++ index.js
@@ -2,17 +2,9 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
-<<<<<<< HEAD
 var request = require('sync-request');
 let APIKEY = "e7c08c6236afd4f414023bbc6db15ae2";
 let baseURL = 'https://api.themoviedb.org/3/';
-=======
-
-let APIKEY = "e7c08c6236afd4f414023bbc6db15ae2";
-let baseURL = 'https://api.themoviedb.org/3/';
-var request = require('sync-request');
-
->>>>>>> 7e6e3cf8fa097bd59a72b5da49b60cc547163d0c
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -32,36 +24,25 @@ const PedirReviewIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PedirReviewIntent';
     },
     handle(handlerInput) {
-<<<<<<< HEAD
         const slots = handlerInput.requestEnvelope.request.intent.slots;
         let movie = slots['movie'].value;
-        var res = request('GET', baseURL + 'search/movie?api_key=' + APIKEY + '&query=' + movie);
-        var json = JSON.parse(res.getBody());
-        var number = json.total_results;
-        let speakOutput = "I'm sorry, I could'nt find that movie.";
-        if (number <= 0) {
-            return handlerInput.responseBuilder
-=======
-        let movieName = 'starwars';
-        let res = request('GET', baseURL + 'search/movie?api_key=' + APIKEY + '&query=' + movieName);
-        let num = JSON.parse(res.getBody());
-        const speakOutput = num.results[0].id;
-        return handlerInput.responseBuilder
->>>>>>> 7e6e3cf8fa097bd59a72b5da49b60cc547163d0c
-            .speak(speakOutput)
-            .reprompt()
-            .getResponse();
+        let speakOutput = "I didn't quite get that, try again, please."
+        if (movie !== null){
+            var res = request('GET', baseURL + 'search/movie?api_key=' + APIKEY + '&query=' + movie);
+            var json = JSON.parse(res.getBody());
+            var number = json.total_results;
+            speakOutput = "I'm sorry, I could'nt find the movie " + movie;
+            if (number > 0) {
+                let id = json.results[0].id;
+                var reviewRes = request('GET', baseURL + 'movie/' + id + '/reviews?api_key=' + APIKEY + '&language=en-US');
+                var jsonReview = JSON.parse(reviewRes.getBody());
+                speakOutput = "I'm sorry, I could'nt find any review for the movie " + json.results[0].title;
+                if (jsonReview.total_results > 0) {
+                    speakOutput = 'This is one review of ' + json.results[0].title + ', ' + jsonReview.results[0].content;
+                }
+            }
         }
-        let id = json.results[0].id;
-        var reviewRes = request('GET', baseURL + 'movie/' + id + '/reviews?api_key=' + APIKEY + '&language=en-US');
-        var jsonReview = JSON.parse(reviewRes.getBody());
-        if (jsonReview.total_results <= 0) {
-            return handlerInput.responseBuilder
-            .speak("I'm sorry, I could'nt find any review for that movie.")
-            .reprompt()
-            .getResponse();
-        }
-        speakOutput = 'This is one review of ' + json.results[0].title + ', ' + jsonReview.results[0].content;
+        
         return handlerInput.responseBuilder
         .speak(speakOutput)
         .reprompt('Anything else?')
